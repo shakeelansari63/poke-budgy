2dca91410bb94751e2a2d94918af5f6170336827
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2025-10-28 20:25:03 +0530

    Fix issues in new SDK

--- components/budget-spend-dialog.tsx
+++ components/budget-spend-dialog.tsx
@@ -1,5 +1,12 @@
 import { ScrollView } from "react-native";
-import { Button, TextInput, Surface, Card, IconButton, useTheme } from "react-native-paper";
+import {
+  Button,
+  TextInput,
+  Surface,
+  Card,
+  IconButton,
+  useTheme,
+} from "react-native-paper";
 import { useState, RefObject } from "react";
 import { DatePickerInput } from "react-native-paper-dates";
 import { useDispatch } from "react-redux";
@@ -10,124 +17,143 @@ import { Expense } from "../model/expense";
 import { useCurrencySymbol } from "../hooks/use-settings";
 
 interface EditBudgetSpendDialogProps {
-    expense?: Expense;
-    categoryId: string;
-    sheetRef: RefObject<BottomSheetModal | null>;
+  expense?: Expense;
+  categoryId: string;
+  sheetRef: RefObject<BottomSheetModal | null>;
 }
 
-const EditBudgetSpendDialog = ({ expense, categoryId, sheetRef }: EditBudgetSpendDialogProps) => {
-    const parsedDate = typeof expense?.ExpenseDate === "string" ? new Date(expense.ExpenseDate) : expense?.ExpenseDate;
-    const [expenseComment, setExpenseComment] = useState(expense?.Comment ?? "");
-    const [expenseAmount, setExpenseAmount] = useState(expense?.Amount ?? 0);
-    const [expenseDate, setExpenseDate] = useState(parsedDate ?? new Date());
-    const [dirty, setDirty] = useState(false);
-    const currencySymbol = useCurrencySymbol();
+const EditBudgetSpendDialog = ({
+  expense,
+  categoryId,
+  sheetRef,
+}: EditBudgetSpendDialogProps) => {
+  const parsedDate =
+    typeof expense?.ExpenseDate === "string"
+      ? new Date(expense.ExpenseDate)
+      : expense?.ExpenseDate;
+  const [expenseComment, setExpenseComment] = useState(expense?.Comment ?? "");
+  const [expenseAmount, setExpenseAmount] = useState(expense?.Amount ?? 0);
+  const [expenseDate, setExpenseDate] = useState(parsedDate ?? new Date());
+  const [dirty, setDirty] = useState(false);
+  const currencySymbol = useCurrencySymbol();
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const theme = useTheme();
+  const theme = useTheme();
 
-    const resetForm = () => {
-        setExpenseComment("");
-        setExpenseAmount(0);
-        setExpenseDate(new Date());
-    };
+  const resetForm = () => {
+    setExpenseComment("");
+    setExpenseAmount(0);
+    setExpenseDate(new Date());
+  };
 
-    const addExpenseHandler = () => {
-        if (!dirty) return;
-        dispatch(
-            addExpense({
-                expense: { Id: "", Comment: expenseComment, ExpenseDate: expenseDate, Amount: expenseAmount },
-                categoryId: categoryId,
-            })
-        );
-        sheetRef.current?.dismiss();
-        resetForm();
-    };
-
-    const updateExpenseHandler = () => {
-        if (!dirty) return;
-        dispatch(
-            editExpense({
-                expense: {
-                    ...expense,
-                    Comment: expenseComment,
-                    ExpenseDate: expenseDate,
-                    Amount: expenseAmount,
-                },
-                categoryId: categoryId,
-            })
-        );
-        sheetRef.current?.dismiss();
-    };
-
-    return (
-        <Dialog sheetRef={sheetRef}>
-            <Surface mode="flat" style={{ marginVertical: 10, marginHorizontal: 20 }}>
-                <Card.Title
-                    title={expense === undefined ? "Add Expense" : "Edit Expense"}
-                    titleVariant="titleLarge"
-                    right={() => <IconButton icon="close" onPress={() => sheetRef.current?.dismiss()} />}
-                />
-                <Card.Content>
-                    <ScrollView>
-                        <TextInput
-                            mode="outlined"
-                            label="Comment"
-                            defaultValue={expenseComment}
-                            onChangeText={(txt) => {
-                                setDirty(true);
-                                setExpenseComment(txt);
-                            }}
-                            style={{ marginBottom: 10 }}
-                            render={(props) => <BottomSheetTextInput {...props} />}
-                        />
-                        <TextInput
-                            mode="outlined"
-                            label="Amount"
-                            keyboardType="numeric"
-                            defaultValue={expenseAmount === 0 ? "" : expenseAmount.toString()}
-                            onChangeText={(text) => {
-                                const number = text.replace(/[^0-9\.]/g, "");
-                                const updateVal = number === "" ? 0 : parseFloat(number);
-                                setDirty(true);
-                                setExpenseAmount(updateVal);
-                            }}
-                            style={{ marginBottom: 10 }}
-                            render={(props) => <BottomSheetTextInput {...props} />}
-                            left={<TextInput.Affix text={currencySymbol} />}
-                        />
-                        <DatePickerInput
-                            mode="outlined"
-                            locale="en"
-                            label="Income Date"
-                            value={expenseDate}
-                            onChange={(d) => {
-                                if (d !== undefined) {
-                                    setDirty(true);
-                                    setExpenseDate(d);
-                                }
-                            }}
-                            inputMode="start"
-                            presentationStyle="pageSheet"
-                            style={{ marginBottom: 10 }}
-                            render={(props) => <BottomSheetTextInput {...props} />}
-                        />
-                    </ScrollView>
-                </Card.Content>
-                <Card.Actions>
-                    <Button
-                        mode="text"
-                        textColor={theme.colors.onPrimaryContainer}
-                        icon="content-save"
-                        onPress={expense === undefined ? addExpenseHandler : updateExpenseHandler}
-                    >
-                        Save
-                    </Button>
-                </Card.Actions>
-            </Surface>
-        </Dialog>
+  const addExpenseHandler = () => {
+    if (!dirty) return;
+    dispatch(
+      addExpense({
+        expense: {
+          Id: "",
+          Comment: expenseComment,
+          ExpenseDate: expenseDate,
+          Amount: expenseAmount,
+        },
+        categoryId: categoryId,
+      }),
     );
+    sheetRef.current?.dismiss();
+    resetForm();
+  };
+
+  const updateExpenseHandler = () => {
+    if (!dirty) return;
+    dispatch(
+      editExpense({
+        expense: {
+          ...expense,
+          Comment: expenseComment,
+          ExpenseDate: expenseDate,
+          Amount: expenseAmount,
+        },
+        categoryId: categoryId,
+      }),
+    );
+    sheetRef.current?.dismiss();
+  };
+
+  return (
+    <Dialog sheetRef={sheetRef}>
+      <Surface mode="flat" style={{ marginVertical: 10, marginHorizontal: 20 }}>
+        <Card.Title
+          title={expense === undefined ? "Add Expense" : "Edit Expense"}
+          titleVariant="titleLarge"
+          right={() => (
+            <IconButton
+              icon="close"
+              onPress={() => sheetRef.current?.dismiss()}
+            />
+          )}
+        />
+        <Card.Content>
+          <ScrollView>
+            <TextInput
+              mode="outlined"
+              label="Amount"
+              keyboardType="numeric"
+              defaultValue={expenseAmount === 0 ? "" : expenseAmount.toString()}
+              onChangeText={(text) => {
+                const number = text.replace(/[^0-9\.]/g, "");
+                const updateVal = number === "" ? 0 : parseFloat(number);
+                setDirty(true);
+                setExpenseAmount(updateVal);
+              }}
+              style={{ marginBottom: 10 }}
+              render={(props) => <BottomSheetTextInput {...props} />}
+              left={<TextInput.Affix text={currencySymbol} />}
+            />
+            <TextInput
+              mode="outlined"
+              label="Comment"
+              defaultValue={expenseComment}
+              onChangeText={(txt) => {
+                setDirty(true);
+                setExpenseComment(txt);
+              }}
+              style={{ marginBottom: 10 }}
+              render={(props) => <BottomSheetTextInput {...props} />}
+            />
+            <DatePickerInput
+              mode="outlined"
+              locale="en"
+              label="Income Date"
+              value={expenseDate}
+              onChange={(d) => {
+                if (d !== undefined) {
+                  setDirty(true);
+                  setExpenseDate(d);
+                }
+              }}
+              inputMode="start"
+              presentationStyle="pageSheet"
+              style={{ marginBottom: 10 }}
+              render={(props) => <BottomSheetTextInput {...props} />}
+            />
+          </ScrollView>
+        </Card.Content>
+        <Card.Actions>
+          <Button
+            mode="text"
+            textColor={theme.colors.onPrimaryContainer}
+            icon="content-save"
+            onPress={
+              expense === undefined ? addExpenseHandler : updateExpenseHandler
+            }
+          >
+            Save
+          </Button>
+        </Card.Actions>
+      </Surface>
+    </Dialog>
+  );
 };
 
 export default EditBudgetSpendDialog;
